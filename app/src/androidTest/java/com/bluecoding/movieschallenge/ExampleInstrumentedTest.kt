2c61ba9f06b7b806a38e06daa6a20449ee3416c9
rafaclement <clement.rafael@gmail.com>
2019-08-19 11:06:47 -0300

    setup project

---
+++ app/src/androidTest/java/com/bluecoding/movieschallenge/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.bluecoding.movieschallenge
+
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.bluecoding.movieschallenge", appContext.packageName)
+    }
+}
